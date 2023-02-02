10d487b093ddc5d6b9a3098b19b7ee9a8e6844b1
TWZ21 <tanweizhong15@hotmail.com>
2023-02-02 10:49:41 +0800

    Some Pages
    Some Pages

---
+++ client/routing.js
@@ -0,0 +1,18 @@
+// Routing betwwen pages in application 
+
+// export default function Links() {
+//     const [user, setUser] = useState("");
+  
+//     return (
+//       <>
+//         <UserContext.Provider value={{ user }}>
+//           <Routes>
+//             <Route path="/" element={user ? <LandingPage /> : <MainPage />} />
+//             <Route path="/LoginRegisterPage" element={<LoginRegisterPage />} />
+//             <Route path="/LandingPage" element={<LandingPage />} />
+//             <Route path="/SuccessPage" element={<SuccessPage />} />
+//           </Routes>
+//         </UserContext.Provider>
+//       </>
+//     );
+//   }
