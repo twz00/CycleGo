739c0da6237c97c338b6becf8c8565cb1c9ad694
TWZ21 <tanweizhong15@hotmail.com>
2023-02-03 23:42:34 +0800

    Routing
    Routing

--- client/routing.js
+++ client/routing.js
@@ -1,18 +1,25 @@
-// Routing betwwen pages in application 
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Layout from "./pages/Layout";
+// import Home from "./pages/Home";
+// import Blogs from "./pages/Blogs";
+// import Contact from "./pages/Contact";
+// import NoPage from "./pages/NoPage";
 
-// export default function Links() {
-//     const [user, setUser] = useState("");
-  
-//     return (
-//       <>
-//         <UserContext.Provider value={{ user }}>
-//           <Routes>
-//             <Route path="/" element={user ? <LandingPage /> : <MainPage />} />
-//             <Route path="/LoginRegisterPage" element={<LoginRegisterPage />} />
-//             <Route path="/LandingPage" element={<LandingPage />} />
-//             <Route path="/SuccessPage" element={<SuccessPage />} />
-//           </Routes>
-//         </UserContext.Provider>
-//       </>
-//     );
-//   }
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Layout />}>
+//           <Route index element={<Home />} />
+//           <Route path="blogs" element={<Blogs />} />
+//           <Route path="contact" element={<Contact />} />
+//           <Route path="*" element={<NoPage />} />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
